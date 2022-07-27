04537af8b2ac70d75a39a89b2c8ccb02a5debd4d
jacekwosz <62403718+jacekwosz@users.noreply.github.com>
2022-07-27 11:48:01 -0700

    Create testdb.php

---
+++ src/testdb.php
@@ -0,0 +1,6 @@
+<?php
+printf ("Format %%f (rzeczywisty zmiennopozycyjny)  dla liczby -16 = %f <br>",-16);
+printf ("Format %%X (szesnastkowy) dla liczby 255 - %X <br>",255);
+printf ("Format %%d (całkowity ze znakiem) dla liczby -16 i 16 : %d i %d <br>",-16, 16);
+printf ("Format %%e (wykładniczy) dla liczb:<br> 1000000 = %e <br> 0.000001 = %e<br>",1000000,0.000001);
+?>
