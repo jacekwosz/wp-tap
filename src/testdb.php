287de8e50af25688f79620729f08e7960f68b53a
jacekwosz <62403718+jacekwosz@users.noreply.github.com>
2022-07-27 23:50:35 -0700

    Update testdb.php

--- src/testdb.php
+++ src/testdb.php
@@ -1,8 +1,15 @@
 <?php
-printf ("Format %%f (rzeczywisty zmiennopozycyjny)  dla liczby -16 = %f <br>",-16);
-printf ("Format %%X (szesnastkowy) dla liczby 255 - %X <br>",255);
-printf ("Format %%d (całkowity ze znakiem) dla liczby -16 i 16 : %d i %d <br>",-16, 16);
-printf ("Format %%e (wykładniczy) dla liczb:<br> 1000000 = %e <br> 0.000001 = %e<br>",1000000,0.000001);
+define( 'DB_NAME', strval(fopen( '/bindings/db/database', 'r')));
+
+/** MySQL database username */
+define( 'DB_USER', strval(fopen('/bindings/db/username', 'r' )));
+
+/** MySQL database password */
+define( 'DB_PASSWORD', strval(fopen('/bindings/db/password', 'r')));
+
+/** MySQL hostname */
+define( 'DB_HOST', strval(fopen('/bindings/db/host', 'r')));
+
 printf ( '$DB_NAME');
 
 /** MySQL database username */
