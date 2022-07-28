dc146523da72a5abaa552d8e9a318b7f204a92d7
jacekwosz <62403718+jacekwosz@users.noreply.github.com>
2022-07-27 23:54:51 -0700

    Update testdb.php

--- src/testdb.php
+++ src/testdb.php
@@ -10,13 +10,13 @@ define( 'DB_PASSWORD', strval(fopen('/bindings/db/password', 'r')));
 /** MySQL hostname */
 define( 'DB_HOST', strval(fopen('/bindings/db/host', 'r')));
 
-printf ( '$DB_NAME');
+printf (DB_NAME);
 
 /** MySQL database username */
-printf( '$DB_USER');
+printf( DB_USER;
 
 /** MySQL database password */
-printf( '$DB_PASSWORD');
+printf( 'DB_PASSWORD');
 
 /** MySQL hostname */
 printf ( '$DB_HOST') 
