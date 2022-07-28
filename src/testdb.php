8445c73f895c358761b75b9bb56841ca2cd32579
jacekwosz <62403718+jacekwosz@users.noreply.github.com>
2022-07-28 01:49:18 -0700

    Update testdb.php

--- src/testdb.php
+++ src/testdb.php
@@ -1,14 +1,14 @@
 <?php
-define( 'DB_NAME', strval(fopen( '/bindings/db/database', 'r')));
+define( 'DB_NAME', strval(fread( '/bindings/db/database', 'r')));
 
 /** MySQL database username */
-define( 'DB_USER', strval(fopen('/bindings/db/username', 'r' )));
+define( 'DB_USER', strval(fread('/bindings/db/username', 'r' )));
 
 /** MySQL database password */
-define( 'DB_PASSWORD', strval(fopen('/bindings/db/password', 'r')));
+define( 'DB_PASSWORD', strval(fread('/bindings/db/password', 'r')));
 
 /** MySQL hostname */
-define( 'DB_HOST', strval(fopen('/bindings/db/host', 'r')));
+define( 'DB_HOST', strval(fread('/bindings/db/host', 'r')));
 
 printf (DB_NAME);
 
